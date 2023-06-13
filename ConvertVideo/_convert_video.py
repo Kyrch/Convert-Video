a49ffde4f0a68eabad91cf6c6412eb64b9e8967a
Kyrch <kyrchk5@gmail.com>
2023-06-13 12:11:19 -0300

    fix: function not working

--- ConvertVideo/_convert_video.py
+++ ConvertVideo/_convert_video.py
@@ -5,7 +5,7 @@ def convert_video():
     input_file = sys.argv[1]
     format = input("Choose a format to convert (e.g. mp4, webm): ")
     output_file = input_file.split('.')[0] + "." + format
-    command = f'ffmpeg -i "{input_file}" -vcodec copy -acodec copy "{output_file}"'
+    command = f'ffmpeg -i "{input_file}" "{output_file}"'
     subprocess.call(command, shell=True)
 
 convert_video()
--- ConvertVideo/_create_key.py
+++ ConvertVideo/_create_key.py
@@ -7,7 +7,8 @@ class RegisterKey:
 
     def __init__(self):
         self.command = RegisterKey.create_command()
-        self.context = RegisterKey.add_context_menu_to_formats(self)
+        self.context_menu = RegisterKey.create_context_menu(self)
+        self.context_for_formats = RegisterKey.add_context_menu_to_formats(self)
 
     def create_command():
         python_path = os.path.dirname(sys.executable) + '\python.exe'
@@ -18,18 +19,18 @@ class RegisterKey:
 
     def create_context_menu(self):
         key_path = r"Software\Classes\SystemFileAssociations\video\shell\ConvertVideo\Command"
-        key = winreg.CreateKey(winreg.HKEY_CLASSES_ROOT, key_path)
+        key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, key_path)
 
         winreg.SetValueEx(key, "", 0, winreg.REG_SZ, self.command)
         winreg.CloseKey(key)
-        RegisterKey.add_context_menu_to_formats()
+
+        subprocess.call(["ie4uinit.exe", "-show"])
 
     def add_context_menu_to_formats(self):
         formats = ['.webm', '.mp4', '.mkv', '.mov', '.avi', '.wmv', '.avchd', '.flv', '.f4v', '.swf', '.m2ts']
 
         for format in formats:
             key_path = fr"Software\Classes\SystemFileAssociations\{format}\shell\ConvertVideo\Command"
-
             key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, key_path)
 
             winreg.SetValueEx(key, "", 0, winreg.REG_SZ, self.command)
